84c465c89ae3243319ac7fd88ee62b83b7409485
Triskalimatu20 <triskalimatu@gmail.com>
2025-09-30 11:34:58 +0700

    first commit

---
+++ array_2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Data Dosen</title>
+        <style>
+            table, th, td {
+                border: 1px solid black;
+                border-collapse: collapse;
+                padding: 5px;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <?php
+        $Dosen = [
+            'nama' => 'Elok Nur Hamdia',
+            'domisili' => 'Malang',
+            'jenis_kelamin' => 'Perempuan' 
+        ];
+        ?>
+
+        <table>
+            <tr>
+                <th>Field</th>
+                <th>Data</th>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td><?php echo $Dosen['nama']; ?></td>
+            </tr>
+            <tr>
+                <td>Domisili</td>
+                <td><?php echo $Dosen['domisili']; ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+            </tr>
+        </table>
+    </body>
+</html>
